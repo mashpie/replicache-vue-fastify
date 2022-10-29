ddd420c7403dd74fbae42dad65be407e8222326d
Marcus Spiegel <spiegel@uscreen.de>
2022-10-29 14:44:44 +0200

    cleanups

--- client/todo/src/components/TaskItem.vue
+++ client/todo/src/components/TaskItem.vue
@@ -1,22 +1,22 @@
 <template>
-  <li class="relative flex items-center justify-between border-b px-2 py-4">
-    <label class="select-none cursor-pointer">
-      <div class="flex h-5 items-center">
+  <li class="flex items-center justify-between border-b px-2 py-1">
+    <label class="flex-1 select-none cursor-pointer py-3 w-0">
+      <div class="flex items-center w-full">
         <input
           :checked="task.done"
           type="checkbox"
           class="h-5 w-5 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
           @change="toggleTask(task)"
         />
-        <div class="ml-3 text-md font-medium text-gray-700">
-          {{ task.title }} ({{ task.done }})
+        <div class="ml-3 text-md font-medium text-gray-700 truncate">
+          {{ task.title }}
         </div>
       </div>
     </label>
 
     <button
       type="button"
-      class="rounded-md border border-transparent text-red-300 bg-white-600 px-2.5 py-1 text-base font-medium hover:text-white hover:bg-red-600 focus:outline-none focus:z-10 focus:ring-2 focus:ring-offset-2 focus:ring-red-500 focus:text-red-600"
+      class="rounded-md border border-transparent text-red-300 bg-white-600 px-2.5 py-1 text-sm font-medium hover:text-white hover:bg-red-600 focus:outline-none focus:z-10 focus:ring-2 focus:ring-offset-2 focus:ring-red-500 focus:text-red-600"
       @click="removeTask(task)"
     >
       <span>Delete</span>
--- client/todo/src/states/tasks.js
+++ client/todo/src/states/tasks.js
@@ -15,25 +15,3 @@ export const removeTask = (task) => {
 export const toggleTask = (task) => {
   task.done = !task.done
 }
-
-/**
- * or as array
- */
-
-// import { ref } from 'vue'
-// import { id } from '@rnvf/id-generator'
-
-// export const taskList = ref([])
-
-// export const addTask = (task) => {
-//   taskList.value.push({ ...task, id: id('task'), done: true })
-// }
-
-// export const removeTask = (task) => {
-//   const index = taskList.value.findIndex((t) => t.id === task.id)
-//   taskList.value.splice(index, 1)
-// }
-
-// export const toggleTask = (task) => {
-//   task.done = !task.done
-// }
